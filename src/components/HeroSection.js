4ae254b7bc04ead926e542c7804ae088c939c6d4
daciomarc <dacio.marcon@gmail.com>
2024-09-04 10:45:54 -0300

    modificando parametros

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <section id="hero">
       <h1>Juzgado de Faltas</h1>
-      <p>Defendiendo los derechos de la comunidad</p>
+      <p>Defendiendo los derechos de la comunidad loretana</p>
     </section>
   );
 };
