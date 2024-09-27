b052c6db322060c47bcf7d9f9dad98e7f7262765
daciomarc <dacio.marcon@gmail.com>
2024-09-27 10:56:51 -0300

    cambios menores, progresion

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HeroSection = () => {
   return (
     <section id="hero">
-      <h1>Juzgado de Faltas</h1>
+      <h1>Juzgado de Faltas de la Ciudad de Loreto</h1>
       <p>Defendiendo los derechos de la comunidad loretana</p>
     </section>
   );
