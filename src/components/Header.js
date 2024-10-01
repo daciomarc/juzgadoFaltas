d2402d6ecc3a68170c8b4a022676d5d427da6657
daciomarc <dacio.marcon@gmail.com>
2024-10-01 09:39:36 -0300

    pequeños cambios, añadiendo login

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ const Header = () => {
           <li><a href="#about">Acerca</a></li>
           <li><a href="#services">Servicios</a></li>
           <li><a href="#contact">Contacto</a></li>
+          <li><a href="https://qr.me-qr.com/mobile/pdf/70815ec2-fade-4b75-8c98-588664472f79">Codigo de Faltas</a></li>
         </ul>
       </nav>
     </header>
